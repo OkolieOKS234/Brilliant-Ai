16047a20e229c3e5f8e4c9af852ab622d922a2db
OkolieOKS234 <dokolie040@gmail.com>
2023-07-25 19:27:43 +0100

    changed the name of the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Brilliant',
+  title: 'Davok',
   description: 'Smart Generative AI',
 }
 
