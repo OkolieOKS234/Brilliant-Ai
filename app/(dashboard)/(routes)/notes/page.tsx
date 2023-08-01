c8dabb713d25293c604bab892db0d210c9e79de2
OkolieOKS234 <dokolie040@gmail.com>
2023-08-01 14:40:14 +0100

    added some AI pages

---
+++ app/(dashboard)/(routes)/notes/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notespage = () => {
+  return (
+    <div>
+        <h3 className='text-6xl text-center py-60'>Coming Soon</h3>
+    </div>
+  )
+}
+
+export default Notespage
