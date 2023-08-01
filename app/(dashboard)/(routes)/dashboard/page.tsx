26951e343d06de6f0e3b0a15a07dcbf5b2c45ec7
OkolieOKS234 <dokolie040@gmail.com>
2023-08-01 14:11:17 +0100

    finished the dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,11 @@
 
 "use client";
 
-import { MessageSquare } from "lucide-react";
+import { Card } from "@/components/ui/card";
+import { ArrowRight, Code, Image, MessageSquare, Music, VideoIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+
 
 const tools =[
   {
@@ -11,15 +15,41 @@ color: "text-violet-500",
 bgColor: "bg-violet-500/10",
 href: "/conversation"
   },
-
   {
+    label: "Music Generation",
+    icon: Music,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+    href: "/music"
+      },
+      {
+        label: "Image Generation",
+        icon: Image,
+        color: "text-pink-700",
+        bgColor: "bg-pink-500/10",
+        href: "/music"
+          },
+          {
+            label: "Video Generation",
+            icon: VideoIcon,
+            color: "text-orange-700",
+            bgColor: "bg-orange-500/10",
+            href: "/video"
+              },
+              {
+                label: "Code Generation",
+                icon: Code,
+                color: "text-green-700",
+                bgColor: "bg-green-500/10",
+                href: "/code"
+                  },
+    ]
 
-  }
-]
 
 
 
 export default function DashboardPage() {
+  const router = useRouter();
   return (
   <>  
   <div>
@@ -33,10 +63,31 @@ Explore the power of AI
  </p>
  </div>
 <div className="px-4 md:px-20 lg:px-32 space-y-4">
-{/* {tools.map((tool)=>(
-return
+{tools.map((tool)=>(
+<Card
+onClick={()=>{
+  router.push(tool.href)
+}}
+key={tool.href}
+className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer">
+{/* Icon div */}
+<div className="flex items-center gap-x-4">
+  <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+<tool.icon  className={cn("w-8 h-8", tool.color)}/>
 
-))} */}
+  </div>
+  {/* ENd of ICon */}
+  <div className="font-semibold">
+{tool.label}
+</div>
+</div>
+
+
+<ArrowRight className="w-5 h-5"/>
+
+</Card>
+
+))}
 </div>
 
   </div>
